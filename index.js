98fc34c136f2fbfadaa2fc8871ada3565f961c46
radiodario <dario.villanueva@gmail.com>
2015-03-25 07:46:24 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var aws = require('aws-sdk');
+
+var app = express();
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+app.set('port', process.env.PORT || 5000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET
+
+
+
+app.get('/account', function(req, res){
+    res.render('account.html');
+});
+
+app.get('/sign_s3', function(req, res){
+    aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.s3_object_name,
+        Expires: 60,
+        ContentType: req.query.s3_object_type,
+        ACL: 'public-read'
+    };
+    s3.getSignedUrl('putObject', s3_params, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var return_data = {
+                signed_request: data,
+                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.s3_object_name
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
+});
+
+app.post('/submit_form', function(req, res){
+    title = req.body.title;
+    email = req.body.email;
+    pic_url = req.body.pic_url;
+    update_pic(title, email, pic_url);
+});
+
+
+app.listen(app.get('port'));
+
+
+function update_pic(title, email, pic_url) {
+  // TODO: fill this in
+}
---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+function s3_upload(){
+    var status_elem = document.getElementById("status");
+    var url_elem = document.getElementById("pic_url");
+    var preview_elem = document.getElementById("preview");
+    var s3upload = new S3Upload({
+        file_dom_selector: 'files',
+        s3_sign_put_url: '/sign_s3',
+        onProgress: function(percent, message) {
+            status_elem.innerHTML = 'Upload progress: ' + percent + '% ' + message;
+        },
+        onFinishS3Put: function(public_url) {
+            status_elem.innerHTML = 'Upload completed. Uploaded to: '+ public_url;
+            url_elem.value = public_url;
+            preview_elem.innerHTML = '<img src="'+public_url+'" style="width:300px;" />';
+        },
+        onError: function(status) {
+            status_elem.innerHTML = 'Upload error: ' + status;
+        }
+    });
+}
+
+(function() {
+    var input_element = document.getElementById("files");
+    input_element.onchange = s3_upload;
+})();
